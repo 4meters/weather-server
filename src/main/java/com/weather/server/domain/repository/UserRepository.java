37cc5f0cff6dd08acaa684e181bd844a310f3d14
Marcin <32044@student.pwsztar.edu.pl>
2021-08-27 15:52:50 +0200

    fixed class annotations and api path

--- src/main/java/com/weather/server/domain/repository/UserRepository.java
+++ src/main/java/com/weather/server/domain/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.weather.server.domain.repository;
 
 import com.weather.server.domain.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends MongoRepository<User, String> {
     User findByEmail(String email);
 
