14c4f26c1f8c0fed8fa24e0d0d516605684c6d4b
Marcin Ryznar <mryznar@onet.pl>
2023-05-28 21:03:12 +0200

    allow to pass only part of measure data (if for eg. there is only bme280 sensor)

--- src/main/java/com/weather/server/domain/mapper/NewMeasureMapper.java
+++ src/main/java/com/weather/server/domain/mapper/NewMeasureMapper.java
@@ -14,13 +14,26 @@ public class NewMeasureMapper {
     public Measure mapToEntity(NewMeasureDto newMeasureDto){
         Measure measure = new Measure();
         measure.setStationId(newMeasureDto.getStationId());
-        measure.setDate(Date.from( Instant.parse(newMeasureDto.getDate())));
-        measure.setTemp(Decimal128.parse(newMeasureDto.getTemp()));
-        measure.setPressure(Decimal128.parse(newMeasureDto.getPressure()));
-        measure.setHumidity(Decimal128.parse(newMeasureDto.getHumidity()));
-        measure.setPm10(Decimal128.parse(newMeasureDto.getPm10()));
-        measure.setPm25(Decimal128.parse(newMeasureDto.getPm25()));
-        measure.setPm25Corr(Decimal128.parse(newMeasureDto.getPm25Corr()));
+        measure.setDate(Date.from(Instant.parse(newMeasureDto.getDate())));
+
+        if(newMeasureDto.getTemp()!=null){
+            measure.setTemp(Decimal128.parse(newMeasureDto.getTemp()));
+        }
+        if(newMeasureDto.getHumidity()!=null){
+            measure.setPressure(Decimal128.parse(newMeasureDto.getPressure()));
+        }
+        if(newMeasureDto.getPressure()!=null){
+            measure.setHumidity(Decimal128.parse(newMeasureDto.getHumidity()));
+        }
+        if(newMeasureDto.getPm10()!=null){
+            measure.setPm10(Decimal128.parse(newMeasureDto.getPm10()));
+        }
+        if(newMeasureDto.getPm25()!=null){
+            measure.setPm25(Decimal128.parse(newMeasureDto.getPm25()));
+        }
+        if(newMeasureDto.getPm25Corr()!=null){
+            measure.setPm25Corr(Decimal128.parse(newMeasureDto.getPm25Corr()));
+        }
         
         return measure;
     }
